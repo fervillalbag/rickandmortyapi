5c6ca28cd9181d55e0ee70e3e571e0bf7eeac84e
fervillalbag <21fervillalbag@gmail.com>
2021-03-28 16:15:28 -0400

    vista móvil correctamente en la página del personaje

--- components/SeoTemplate.js
+++ components/SeoTemplate.js
@@ -6,6 +6,7 @@ export default function SeoTemplate() {
     <Head>
       <title>Fetching Data</title>
       <meta name="description" content="Desarrollo con React utilizando la API de Rick and Morty" />
+      <meta name="robots" content="noindex" />
       <link rel="icon" href="/favicon.ico" />
       <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;700&display=swap" rel="stylesheet" />
     </Head>
